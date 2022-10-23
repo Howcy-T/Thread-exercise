6e17f20cac4e95949cb22bc6a7a263df6f5150fa
tanhao <1445187611@qq.com>
2022-10-23 17:48:37 +0800

    sync

---
+++ src/main/java/cn/tamhouse/thread/mutex/SyncCalculate.java
@@ -0,0 +1,50 @@
+package cn.tamhouse.thread.mutex;
+
+/**
+ * @author th
+ * @Descirbe 同步计算 同时加减操作 最后结果为0
+ * @date 2022/10/23 17:11
+ */
+public class SyncCalculate {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Room room=new Room();
+        Thread add = new Thread(room::increment);
+        Thread sub = new Thread(room::decrement);
+        add.start();
+        sub.start();
+        add.join();
+        sub.join();
+        System.out.println(room.getNum());
+    }
+}
+
+
+class Room{
+    private int num=0;
+
+    public void increment(){
+        for (int i = 0; i < 5000; i++) {
+            synchronized (this){
+                num++;
+            }
+
+        }
+    }
+
+    public void decrement(){
+        for (int i = 0; i < 5000; i++) {
+            synchronized (this){
+                num--;
+            }
+
+        }
+    }
+
+    public int getNum() {
+        synchronized (this){
+            return num;
+        }
+    }
+}
