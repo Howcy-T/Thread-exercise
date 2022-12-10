387dda8c8dec6ee05eb7b137cb3d8924ecbac3ef
tanhao <1445187611@qq.com>
2022-12-10 18:32:05 +0800

    顺序打印-wait/notify

---
+++ src/main/java/cn/tamhouse/thread/print/SyncPrint.java
@@ -0,0 +1,35 @@
+package cn.tamhouse.thread.print;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author th
+ * @Descirbe 通过wait/notify的方式顺序打印,要求先打印2 再打印1
+ * @date 2022/12/10 16:38
+ */
+@Slf4j
+public class SyncPrint {
+    static boolean isTwoPrint=false;
+    public synchronized void printOne()  {
+        while (!isTwoPrint){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        log.info("1");
+    }
+
+    public synchronized void printTwo(){
+        log.info("2");
+        isTwoPrint=true;
+        this.notifyAll();
+    }
+
+    public static void main(String[] args) {
+        SyncPrint syncPrint=new SyncPrint();
+        new Thread(syncPrint::printOne,"t1").start();
+        new Thread(syncPrint::printTwo,"t2").start();
+    }
+}
