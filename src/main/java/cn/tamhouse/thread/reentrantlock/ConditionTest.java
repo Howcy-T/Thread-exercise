56a144dfa068066390b3d88d5bd171a96605fe6d
tanhao <1445187611@qq.com>
2022-12-10 16:26:47 +0800

    bug

--- src/main/java/cn/tamhouse/thread/reentrantlock/ConditionTest.java
+++ src/main/java/cn/tamhouse/thread/reentrantlock/ConditionTest.java
@@ -13,73 +13,75 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 @Slf4j
 public class ConditionTest {
-    static boolean hasTakeOut=false;
-    static boolean hasDeliver=false;
-    static ReentrantLock room=new ReentrantLock();
+    static boolean hasTakeOut = false;
+    static boolean hasDeliver = false;
+    static ReentrantLock room = new ReentrantLock();
     //休息室
-    static Condition waitTakeOut=room.newCondition();
-    static Condition waitDeliver=room.newCondition();
+    static Condition waitTakeOut = room.newCondition();
+    static Condition waitDeliver = room.newCondition();
+
     public static void main(String[] args) throws InterruptedException {
-        new Thread(()->{
+        new Thread(() -> {
             room.lock();
-            while (!hasTakeOut){
-                log.info("外卖没到直接开摆...");
-                try {
-                    waitTakeOut.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
             try {
+                while (!hasTakeOut) {
+                    log.info("外卖没到直接开摆...");
+                    try {
+                        waitTakeOut.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 log.info("外卖到了，开始干活...");
             } finally {
                 room.unlock();
             }
-        },"小明").start();
+        }, "小明").start();
 
 
-
-        new Thread(()->{
+        new Thread(() -> {
             room.lock();
-            while (!hasDeliver){
-                log.info("快递还不到，sb顺丰");
-                try {
-                    waitDeliver.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
             try {
+                while (!hasDeliver) {
+                    log.info("快递还不到，sb顺丰");
+                    try {
+                        waitDeliver.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 log.info("快递总算到了啊");
             } finally {
                 room.unlock();
             }
-        },"小红").start();
+        }, "小红").start();
 
 
-        new Thread(()->{
+        new Thread(() -> {
             room.lock();
             try {
-                hasTakeOut=true;
+                hasTakeOut = true;
                 waitTakeOut.signalAll();
                 log.info("外卖已送达");
             } finally {
                 room.unlock();
             }
-        },"外卖小哥").start();
+        }, "外卖小哥").start();
 
         TimeUnit.SECONDS.sleep(2);
 
-        new Thread(()->{
+        new Thread(() -> {
             room.lock();
             try {
-                hasDeliver=true;
+                hasDeliver = true;
                 waitDeliver.signalAll();
                 log.info("快递已送达");
             } finally {
                 room.unlock();
             }
-        },"顺丰").start();
+        }, "顺丰").start();
 
     }
 }
